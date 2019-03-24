32cadcc59c4733c5dd1d3248314d4454cf33d120
Jacob Knudson <jake.knudson@knights.ucf.edu>
2019-03-24 02:03:46 -0400

    finally adding actual build folder

---
+++ client/build/precache-manifest.34adaf89126bff814143ac1570f4208d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1f21767433b237dd4ab",
+    "url": "/static/css/main.75111c57.chunk.css"
+  },
+  {
+    "revision": "d1f21767433b237dd4ab",
+    "url": "/static/js/main.d1f21767.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b3b05b54c4fa94f56faf",
+    "url": "/static/css/2.d6488c72.chunk.css"
+  },
+  {
+    "revision": "b3b05b54c4fa94f56faf",
+    "url": "/static/js/2.b3b05b54.chunk.js"
+  },
+  {
+    "revision": "eb0dc2bba354bcfd2e5440f4a288c5ff",
+    "url": "/index.html"
+  }
+];
